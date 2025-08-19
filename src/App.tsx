33ac6cfa9da34d25692138993851bb3d5599755e
Miguel243 <miguelitocap12@gmail.com>
2025-08-19 19:59:59 -0300

    feat: update Button component to use children prop and enhance App layout with Counter and Buttons

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,24 @@
-import './index.css'
-import Layout from './components/Layout';
+import "./index.css";
+import Layout from "./components/Layout";
 import Filters from "./components/Filters";
 import { Counter } from "./components/Counter";
+import { Button } from "./components/Button";
+import { useState } from "react";
 
 function App() {
-
-  return (
-    <>
-      <Layout>
-        <h1 className="text-4xl font-bold text-white">Hello World</h1>
-         <Filters name={"All"} />
-      </Layout>
-    </>
-  )
+	const [quote, setQuote] = useState<number>(0);
+	return (
+		<>
+			<Layout>
+				<Counter count={quote} />
+				<Button setCount={setQuote}>boton 1</Button>
+				<Button setCount={setQuote}>boton 2</Button>
+				<Button>boton 3</Button>
+				<h1 className="text-4xl font-bold text-white">Hello World</h1>
+				<Filters name={"All"} />
+			</Layout>
+		</>
+	);
 }
 
 export default App;
