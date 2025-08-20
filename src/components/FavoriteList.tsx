2baa29e5cc5535f0634d341e15eeb92d48d189ce
celeste courtade <celestecmex@gmail.com>
2025-08-20 01:45:44 +0000

    delete file

--- src/components/FavoriteList.tsx
+++
@@ -1,8 +0,0 @@
-export default function FavoriteList() {
-  return (
-    <aside className="bg-indigo-900 p-4 rounded-lg">
-      <h2 className="text-2xl font-bold text-white">Favorite List</h2>
-      {/* Aquí puedes agregar la lógica para mostrar la lista de favoritos */}
-    </aside>
-  );
-}
