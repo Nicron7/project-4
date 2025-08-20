5cb0c848141c4d87f85b555e589df18ffcba77c0
celeste courtade <celestecmex@gmail.com>
2025-08-20 00:14:48 +0000

    quotes

--- src/components/Counter.tsx
+++ src/components/Counter.tsx
@@ -1,8 +1,11 @@
 interface CounterProps {
-    count: number
+  count: number;
 }
 
 export const Counter = ({ count }: CounterProps) => {
-  return <div>Quotes: {count}</div>;
+  return (
+    <div className="bg-blue-500 text-white font-semibold px-2 py-2 rounded m-2">
+      Quotes: {count}
+    </div>
+  );
 }
-
