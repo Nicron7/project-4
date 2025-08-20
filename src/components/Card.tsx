88601f2c1e23e5a4f9f015e6b3a9cf0ae3de4e1e
Nicron <nicron007@gmail.com>
2025-08-20 00:39:51 +0000

    style: add some styles

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -5,9 +5,11 @@ type CardProps = {
 
 const Card: React.FC<CardProps> = ({ sentence, author }) => {
   return (
-    <div className="card bg-indigo-900 px-10 py-20 text-center">
-      <h2 className="text-2xl font-bold text-indigo-50 mb-5">"{sentence}"</h2>
-      <p className="text-xl text-white font-light">-- {author}</p>
+    <div className="card bg-indigo-900 px-10 py-20 text-center shadow-lg">
+      <h2 className="text-2xl font-medium text-indigo-50 mb-5 italic">
+        "{sentence}"
+      </h2>
+      <p className="text-xl text-indigo-200 font-light">— {author}</p>
     </div>
   );
 };
